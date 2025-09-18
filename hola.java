17cfed4ed52a2b55a8beaa5798814e43f899708b
Junior Pérez <jrplox987@gmail.com>
2025-09-18 17:42:55 -0500

    Agregada nueva función

--- hola.java
+++ hola.java
@@ -1,5 +1,15 @@
-public class Hola {
+public class hola {
     public static void main(String[] args) {
-        System.out.println("Hola Backend desde Git!");
+        int valor = 20;
+        int valor2 = 30;
+
+        System.out.println(funcionSuma(valor, valor2));
+    }
+
+    public static String funcionSuma(int valorx, int valorY){
+            
+            int suma = valorx + valorY;
+
+            return "La suma de estos dos valores es:" + suma;
     }
 }
