17d58ebef97eaebc1aea49735842fd396cd96848
Junior Pérez <jrplox987@gmail.com>
2025-09-18 19:52:57 -0500

    Segundo commit: clase Hola.java creada

--- src/main/java/hola.java
+++ src/main/java/hola.java
@@ -4,13 +4,14 @@ public class hola {
     public static void main(String[] args) {
         int valor = 200;
         int valor2 = 50;
+        int valor3 =60;
 
-        System.out.println(funcionSuma(valor, valor2));
+        System.out.println(funcionSuma(valor, valor2, valor3));
     }
 
-    public static String funcionSuma(int valorx, int valorY){
+    public static String funcionSuma(int valorx, int valorY, int valorZ){
 
-        int suma = valorx * valorY;
+        int suma = valorx * valorY * valorZ;
 
         return "La multiplicación de estos dos valores es: " + suma;
     }
